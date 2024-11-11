735498b0bef14f7f8ec418992d5478893e49db3f
SkanderAyoub <skanderayoub@live.fr>
2024-11-11 18:53:35 +0100

    correct scraper

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let jsonData;
+let musicData;
 let firstClick = true;
 
 // Step 1: Fetch the data asynchronously and store it in jsonData
@@ -15,13 +16,26 @@ async function fetchAudioData() {
   }
 }
 
+async function fetchMusicData() {
+  try {
+    const response = await fetch('https://raw.githubusercontent.com/skanderayoub/imissmyhogwarts2/refs/heads/main/music.json');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    musicData = data; // Set jsonData here for global access    
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+
 // Step 2: Get a random key-value pair from jsonData
-function getRandomKeyAndValue() {
-  const keys = Object.keys(jsonData);
+function getRandomKeyAndValue(data) {
+  const keys = Object.keys(data);
   const randomKey = keys[Math.floor(Math.random() * keys.length)];
 
   // Access the value associated with that key
-  const valueList = jsonData[randomKey];
+  const valueList = data[randomKey];
 
   // Select a random value from the list (if the value is an array)
   const randomValue = valueList[Math.floor(Math.random() * valueList.length)];
@@ -33,7 +47,7 @@ function getRandomKeyAndValue() {
 function playSound() {
   let audio = document.getElementById("audio");
   if (firstClick) {
-    const { randomKey, randomValue } = getRandomKeyAndValue();
+    const { randomKey, randomValue } = getRandomKeyAndValue(jsonData);
     setCharacterAndAudio(randomKey, randomValue);
     firstClick = false;
     audio.src = randomValue;
@@ -50,7 +64,7 @@ function playSound() {
 
 function playNewSound() {
   let audio = document.getElementById("audio");
-  const { randomKey, randomValue } = getRandomKeyAndValue();
+  const { randomKey, randomValue } = getRandomKeyAndValue(jsonData);
   setCharacterAndAudio(randomKey, randomValue);
   audio.src = randomValue; // Set the audio source to the random value (e.g., audio URL)
 
@@ -60,6 +74,9 @@ function playNewSound() {
 
 function playMusic() {
   let audio = document.getElementById("audio2");
+  const { randomKey, randomValue } = getRandomKeyAndValue(musicData);
+  setAlbumAndAudio(randomKey, randomValue);
+  audio.src = randomValue; // Set the audio source to the random value (e.g., audio URL)
   audio.play();
 }
 
@@ -80,34 +97,37 @@ function setCharacterAndAudio(randomKey, randomValue) {
 function setAlbumAndAudio(randomKey, randomValue) {
   let album = document.getElementById("p2");
   let audio = document.getElementById("a2");
-  album.innerHTML = "Personnage: " + randomKey;  
+  album.innerHTML = "Musique: " + randomKey;  
   audio.innerHTML = "Audio: " + randomValue; 
 } 
 
 // Step 4: Fetch the data, then set up button event listener for dynamic random value selection
 fetchAudioData().then(() => {
   if (jsonData) {
-    // Get the button and audio elements
-    const playButton = document.getElementById("playButton");
-    const audio = document.getElementById("audio");
-    const music = document.getElementById("audio2");
-    const playMusicButton = document.getElementById("playButton2");
+    fetchMusicData().then(() => {
+      if (musicData) {    
+        // Get the button and audio elements
+        const playButton = document.getElementById("playButton");
+        const audio = document.getElementById("audio");
+        const playMusicButton = document.getElementById("playButton2");
 
-    playMusicButton.addEventListener("click", () => {
-      playMusic();
-    });
+        playMusicButton.addEventListener("click", () => {
+          playMusic();
+        });
 
-    // Add event listener to the button to play sound on click
-    playButton.addEventListener("click", () => {
-      // Get a new random value when the button is clicked and start playing
-      
-      playSound();
-    });
+        // Add event listener to the button to play sound on click
+        playButton.addEventListener("click", () => {
+          // Get a new random value when the button is clicked and start playing
+          
+          playSound();
+        });
 
-    // Listen for when the current audio ends, and play a new sound
-    audio.addEventListener('ended', () => {
-      // When the audio ends, get a new random sound and play it
-      playNewSound();
+        // Listen for when the current audio ends, and play a new sound
+        audio.addEventListener('ended', () => {
+          // When the audio ends, get a new random sound and play it
+          playNewSound();
+        });
+      }
     });
   }
 });
